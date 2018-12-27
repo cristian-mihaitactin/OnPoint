d434c6a3cccf44d2df45568d7b3887dd9d09c819
cristian.mihaitactin <cristian_mihaita.ctin@yahoo.com>
2018-12-27 23:06:36 +0200

    Add smtp email sender and tests

---
+++ eMMA/4.2.1/aspnet-core/src/eMMA.EmailProvider/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace eMMA.EmailProvider
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
