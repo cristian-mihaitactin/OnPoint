f4cd2b6be99393dbb412fe00af7abecc7b44a9e0
Sabina Tumurug <sabina.tumurug@outlook.com>
2019-01-04 13:01:33 +0200

    completed Question and Answer controllers

--- eMMA-vers2/eMMA/Data.Layer/Models/Answer.cs
+++ eMMA-vers2/eMMA/Data.Layer/Models/Answer.cs
@@ -6,7 +6,7 @@ namespace DataLayer.Models
 {
     public class Answer
     {
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
         public Guid StudentId { get; private set; }
         public Guid QuestionId { get; private set; }
         public string AnswerText { get; private set; }
